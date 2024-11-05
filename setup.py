f84af89ab55abc9e4bfc53f404d11850e0f7938c
navdeep-G <mr.navdeepgill@gmail.com>
2024-11-05 07:27:07 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dataset_quality_checker",
+    version="0.1.0",
+    description="A tool for checking data quality in datasets",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=["pandas"],
+)
